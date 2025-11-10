980a63a35a8a9cdce526c6f7d616f5d85f679b2e
lucas-lfm <mendes.lucas.lfm@gmail.com>
2025-11-10 20:31:07 -0300

    Adiciona implementação para verificação de status de domínio com integração à API BrasilAPI

---
+++ exemplos/ex-11-04/index.js
@@ -0,0 +1,50 @@
+// Na sua implementação, você deve pegar o domínio que o usuário informar no formulário da página HTML.
+// Para fins de exemplo, aqui está um domínio fixo.
+
+const form = document.getElementById("domain-form");
+
+const renderizar = function (dados) {
+  let txtStatus;
+  let listaSugestoes = null;
+
+  if (dados.status === "REGISTERED") {
+    txtStatus = "<p>Domínio registrado! Analise as opções abaixo:</p>";
+    listaSugestoes = document.createElement("ul");
+
+    for (let sugestao of dados.suggestions) {
+      const item = document.createElement("li");
+      item.innerText = sugestao;
+      listaSugestoes.appendChild(item);
+    }
+  } else {
+    txtStatus = "<p>Domínio disponível! Entre em contato.</p>";
+  }
+
+  const divStatus = document.getElementById("status");
+
+  divStatus.innerHTML = txtStatus;
+  divStatus.hidden = false;
+
+  if (listaSugestoes) {
+    divStatus.appendChild(listaSugestoes);
+  }
+};
+
+form.addEventListener("submit", async (evento) => {
+  evento.preventDefault();
+
+  const domain = form.querySelector("#domain-input").value;
+
+  // Fazendo a requisição para a API BrasilAPI para verificar o status do domínio
+  // O uso de 'await' aqui requer que o script seja do tipo módulo (type="module" no HTML)
+  // O await é usado para esperar a resposta da requisição antes de continuar a execução do código.
+  const data = await fetch(
+    `https://brasilapi.com.br/api/registrobr/v1/${domain}`
+  );
+
+  // Convertendo a resposta para JSON
+  // O uso de 'await' aqui também é necessário para esperar a conversão dos dados.
+  const domainInfo = await data.json();
+
+  renderizar(domainInfo);
+});
